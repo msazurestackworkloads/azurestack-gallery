08706d425158433c599b50e4ae779ebdc612e6e9
t-chappl <50643705+t-chappl@users.noreply.github.com>
2019-06-04 11:04:41 -0700

    remove unused env var from script (#74)
    * remove unused env var from script
    
    * remove default for kubernetes image base

--- kubernetes/template/DeploymentTemplates/script.sh
+++ kubernetes/template/DeploymentTemplates/script.sh
@@ -250,9 +250,7 @@ log_level -i "------------------------------------------------------------------
 ENVIRONMENT_NAME=AzureStackCloud
 AUTH_METHOD="client_secret"
 IDENTITY_SYSTEM_LOWER="azure_ad"
-K8S_IMAGE_BASE="mcr.microsoft.com/k8s/azurestack/core/"
 
-log_level -i "AZURE_ENV: $AZURE_ENV"
 log_level -i "ENVIRONMENT_NAME: $ENVIRONMENT_NAME"
 
 log_level -i "------------------------------------------------------------------------"
