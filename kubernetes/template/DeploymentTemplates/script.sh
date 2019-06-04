e340427c8c6a7802ee7a577a092745444aa47b4f
Chad Applegate <t-chappl@microsoft.com>
2019-06-04 11:02:36 -0700

    remove default for kubernetes image base

--- kubernetes/template/DeploymentTemplates/script.sh
+++ kubernetes/template/DeploymentTemplates/script.sh
@@ -250,7 +250,6 @@ log_level -i "------------------------------------------------------------------
 ENVIRONMENT_NAME=AzureStackCloud
 AUTH_METHOD="client_secret"
 IDENTITY_SYSTEM_LOWER="azure_ad"
-K8S_IMAGE_BASE="mcr.microsoft.com/k8s/azurestack/core/"
 
 log_level -i "ENVIRONMENT_NAME: $ENVIRONMENT_NAME"
 
