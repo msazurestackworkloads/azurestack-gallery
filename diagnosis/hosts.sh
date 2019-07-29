18966e614699bb33463fac9ce4f638ba30be3fcd
Bhuvaneswari Santharam <bhsantha@microsoft.com>
2019-07-29 14:26:24 -0700

    Changing the ip range in collect logs for nodes

--- diagnosis/hosts.sh
+++ diagnosis/hosts.sh
@@ -14,11 +14,11 @@ HOSTLIST=$WD/host.list
 rm -f $HOSTLIST
 
 echo "[$(date +%Y%m%d%H%M%S)][INFO] Searching for cluster nodes"
-for ip in 10.240.0.{4..21};
+for ip in 10.240.0.{4..40};
 do
     ping -c 1 -W 1 $ip | grep "64 bytes" | cut -d " " -f 4 | cut -d ":" -f 1 >> $HOSTLIST
 done
-for ip in 10.240.255.{5..12};
+for ip in 10.240.255.{5..40};
 do
     ping -c 1 -W 1 $ip | grep "64 bytes" | cut -d " " -f 4 | cut -d ":" -f 1 >> $HOSTLIST
 done
