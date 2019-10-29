45c0fb2a01ef702ad0d887c427a8abc633ab47f0
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2019-10-29 13:21:37 -0700

    Kubernetes gallery item first release (#151)
    * default to kubernetes 1.14
    
    * deploy using AKSe v0.43.0
    
    * removed "preview" from legal terms
    
    * bump version to 1.0.0
    
    * encoded deployment script

--- kubernetes/template/DeploymentTemplates/script.sh
+++ kubernetes/template/DeploymentTemplates/script.sh
@@ -212,7 +212,7 @@ apt_get_install()
 #####################################################################################
 # start
 
-log_level -i "Starting Kubernetes cluster deployment: v0.5.2"
+log_level -i "Starting Kubernetes cluster deployment: v1.0.0"
 log_level -i "Running script as:  $(whoami)"
 log_level -i "System information: $(uname -a)"
 
