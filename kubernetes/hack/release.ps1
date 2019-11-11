bf70d276f45cfa4cdbda3ef203db870be03fed70
Javier Darsie <jadarsie@microsoft.com>
2019-11-11 11:38:20 -0800

    download packager

--- kubernetes/hack/release.ps1
+++ kubernetes/hack/release.ps1
@@ -1,3 +1,11 @@
 mkdir _out
+
+Invoke-WebRequest aka.ms/azurestackmarketplaceitem -o _out\packager.zip
+Expand-Archive _out\packager.zip -DestinationPath _out
+
+$src = (Get-Location).Path + "\_out\Azure Stack Marketplace Item Generator and Sample\AzureGalleryPackageGenerator\"
+$dst = (Get-Location).Path + "\_out\packager\"
+Move-Item -Path $src -Destination $dst
+
 $manifest = (Get-Location).Path + "\template\manifest.json"
-hack\packager\AzureGalleryPackager.exe -m $manifest -o _out
+_out\packager\AzureGalleryPackager.exe -m $manifest -o _out
