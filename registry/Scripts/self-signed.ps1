8f8b65ffd5d0d5ee643aff3eb5ca4283231b3eed
Rohit Jaini <rjaini@microsoft.com>
2019-12-03 20:57:04 -0800

    Adding SKU information

--- registry/Scripts/self-signed.ps1
+++ registry/Scripts/self-signed.ps1
@@ -1,13 +1,27 @@
 #requires -RunAsAdministrator
 
-# Password and Common Name of the certificate
-$PASSWORD=""
-$CN=""
-# Export path of the certificate
-$PATH=""
+Param
+(
+    [Parameter(Mandatory = $true, HelpMessage = "Common DNS FQDN name to be used .")]
+    [string] $DNSCName,
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate Password to be used to export the cert.")]
+    [string] $CertificateSecret,
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate file export path where f.")]
+    [string] $CertificateFileExportPath
+)
+
+if (-not (Test-Path -Path $CertificateFileExportPath -IsValid))
+{
+    Write-Host "Error: Invlid path syntax for CertificateFileExportPath variable."
+}
+
+if (-not ([IO.Path]::GetExtension($CertificateFileExportPath) -eq '.pfx'))
+{
+    Write-Host "Error: Invlid syntax for CertificateFileExportPath variable. Extension value in path should end with '.pfx'"
+}
 
 # Create a self-signed certificate
-$ssc = New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $CN
+$ssc = New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $DNSCName
 $crt = "cert:\localMachine\my\" + $ssc.Thumbprint
-$pwd = ConvertTo-SecureString -String $PASSWORD -Force -AsPlainText
-Export-PfxCertificate -cert $crt -FilePath $PATH -Password $pwd
+$pwd = ConvertTo-SecureString -String $CertificateSecret -Force -AsPlainText
+Export-PfxCertificate -cert $crt -FilePath $CertificateFileExportPath -Password $pwd
