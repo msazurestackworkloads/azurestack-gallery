14ed4d372c528482cc4cc1aa9835b3a7bbd34023
Rohit Jaini <rjaini@microsoft.com>
2019-12-04 22:04:33 -0800

    remove tag from output

--- registry/Scripts/self-signed.ps1
+++ registry/Scripts/self-signed.ps1
@@ -2,26 +2,26 @@
 
 Param
 (
-    [Parameter(Mandatory = $true, HelpMessage = "Common DNS FQDN name to be used .")]
-    [string] $DNSCName,
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate fully qualified domain name.")]
+    [string] $CertificateCN,
     [Parameter(Mandatory = $true, HelpMessage = "Certificate Password to be used to export the cert.")]
     [string] $CertificateSecret,
-    [Parameter(Mandatory = $true, HelpMessage = "Certificate file export path where f.")]
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate file export path including certificate filename.")]
     [string] $CertificateFileExportPath
 )
 
 if (-not (Test-Path -Path $CertificateFileExportPath -IsValid))
 {
-    Write-Host "Error: Invlid path syntax for CertificateFileExportPath variable."
+    Write-Host "Error: CertificateFileExportPath is not a valid path."
 }
 
 if (-not ([IO.Path]::GetExtension($CertificateFileExportPath) -eq '.pfx'))
 {
-    Write-Host "Error: Invlid syntax for CertificateFileExportPath variable. Extension value in path should end with '.pfx'"
+    Write-Host "Error: Invalid syntax for CertificateFileExportPath variable. Extension value in path should end with '.pfx'"
 }
 
 # Create a self-signed certificate
-$ssc = New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $DNSCName
+$ssc = New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $CertificateCN
 $crt = "cert:\localMachine\my\" + $ssc.Thumbprint
 $pwd = ConvertTo-SecureString -String $CertificateSecret -Force -AsPlainText
 Export-PfxCertificate -cert $crt -FilePath $CertificateFileExportPath -Password $pwd
