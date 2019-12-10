e3d8efa0194b9212d3c1a1b9508959bae0723cc0
Rohit <rjaini@microsoft.com>
2019-12-10 11:01:29 -0800

    Feat: Container registry changes to support update scenario (#173)
    Container registry changes to support update scenario.

--- registry/Scripts/gallery.ps1
+++ registry/Scripts/gallery.ps1
@@ -1,22 +1,77 @@
-# External FQDN of the instance
-$FQDN=""
+#requires -runasadministrator
 
-# Admin Login
-$TENANT_ID=""
-$ADMIN_USER=""
-$ADMIN_PASS=""
+<#
+.Synopsis
+  The script provide functionality to install a marketplace item from given URL.
 
-Add-AzureRmEnvironment -ARMEndpoint "https://adminmanagement.$FQDN/" -Name "AzureStackAdmin"
-$secpasswd = ConvertTo-SecureString $ADMIN_PASS -AsPlainText -Force
-$mycreds = New-Object System.Management.Automation.PSCredential ($ADMIN_USER, $secpasswd) 
-Login-AzureRmAccount -Credential $mycreds -EnvironmentName "AzureStackAdmin" -TenantId $TENANT_ID
+.Description
+  The script provide functionality to install a marketplace item from given URL.
+
+.Parameter AzureStackDomainName
+  Fully qualified domain name of Azure Stack instance.
+
+.Parameter AdminUserName
+  Admin username.
+
+.Parameter AdminPassword
+  Admin password.
+
+.Parameter TenantId
+  Tenant ID of Azure Stack.
+
+.Parameter MarketplaceUri
+  Marketplace package download URL.
+
+.Parameter EnvironmentName
+  Name of the environment.
+
+.Example
+   gallery.ps1 -AzureStackDomainName local.microsoft.com 
+               -AdminUserName admin@localazurestack.onmicrosoft.com 
+               -AdminPassword <Login Password> 
+               -TenantId 00000000-0000-0000-0000-000000000000 
+               -MarketplaceUri https://local.blob.core.windows.net/marketplaceblob/Microsoft.AzureStackDockerContainerRegistry.1.0.0.azpkg
+#>
+Param
+(
+    [Parameter(Mandatory = $true, HelpMessage = "Fully qualified domain name of Azure Stack instance.")]
+    [string] $AzureStackDomainName,
+    [Parameter(Mandatory = $true, HelpMessage = "Admin username.")]
+    [string] $AdminUserName,
+    [Parameter(Mandatory = $true, HelpMessage = "Admin password")]
+    [string] $AdminPassword,
+    [Parameter(Mandatory = $true, HelpMessage = "Tenant ID of Azure Stack.")]
+    [string] $TenantId,
+    [Parameter(Mandatory = $true, HelpMessage = "Marketplace package download URL.")]
+    [string] $MarketplaceUri,
+    [Parameter(Mandatory = $false, HelpMessage = "Name of the environment.")]
+    [string] $EnvironmentName = "AzureStackAdmin"
+)
+
+$environment = Get-AzureRmEnvironment -Name $EnvironmentName 
+if ($null -eq $environment)
+{
+    $armEndPoint="https://adminmanagement.$AzureStackDomainName/"
+    $environment = Add-AzureRmEnvironment -Name $EnvironmentName -ARMEndpoint $armEndPoint -ErrorAction Stop
+}
+else
+{
+    Write-Host "Environment($EnvironmentName) is already in use." 
+}
+
+$secpasswd = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($AdminUserName, $secpasswd) 
+Login-AzureRmAccount -Credential $mycreds -EnvironmentName $EnvironmentName -TenantId $TenantId
 
 # Deploy package
-$PKG_SA_NAME=""
-$PKG_SA_CONTAINER=""
-$PKG_VERSION=""
+#$PKG_SA_NAME=""
+#$PKG_SA_CONTAINER=""
+#$PKG_VERSION=""
+#"https://$PKG_SA_NAME.blob.$AzureStackDomainName/$PKG_SA_CONTAINER/Microsoft.AzureStackDockerContainerRegistry.$PKG_VERSION.azpkg"
 
-Add-AzsGalleryItem -Force -GalleryItemUri "https://$PKG_SA_NAME.blob.$FQDN/$PKG_SA_CONTAINER/Microsoft.AzureStackDockerContainerRegistry.$PKG_VERSION.azpkg"
+Add-AzsGalleryItem -Force -MarketplaceUri $MarketplaceUri
+
+
 
 # Remove package
 # Remove-AzsGalleryItem -Force -Name "Microsoft.AzureStackDockerContainerRegistry.$PKG_VERSION"
--- registry/Scripts/self-signed.ps1
+++ registry/Scripts/self-signed.ps1
@@ -1,27 +1,61 @@
-#requires -RunAsAdministrator
+#requires -runasadministrator
+<#
+.Synopsis
+  The script provide functionality to create self signed certificate.
 
+.Description
+  The script provide functionality to create self signed certificate.
+
+.Parameter CertificateCN
+  Certificate common name.
+
+.Parameter CertificatePassword
+  Certificate Password.
+
+.Parameter CertificateFileExportPath
+  Certificate file export path including certificate filename.
+
+.Example
+   Self-Signed.ps1 -CertificateCN "registry.local.microsoft.com" 
+                   -CertificatePassword <Secret>
+                   -CertificateFileExportPath c:\certificate\registry_cert.pfx
+#>
 Param
 (
-    [Parameter(Mandatory = $true, HelpMessage = "Certificate fully qualified domain name.")]
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate common name.")]
     [string] $CertificateCN,
-    [Parameter(Mandatory = $true, HelpMessage = "Certificate Password to be used to export the cert.")]
-    [string] $CertificateSecret,
+    [Parameter(Mandatory = $true, HelpMessage = "Certificate Password.")]
+    [string] $CertificatePassword,
     [Parameter(Mandatory = $true, HelpMessage = "Certificate file export path including certificate filename.")]
     [string] $CertificateFileExportPath
 )
 
 if (-not (Test-Path -Path $CertificateFileExportPath -IsValid))
 {
-    Write-Host "Error: CertificateFileExportPath is not a valid path."
+    throw "Error: CertificateFileExportPath is not a valid path."
 }
 
-if (-not ([IO.Path]::GetExtension($CertificateFileExportPath) -eq '.pfx'))
+if (Test-Path -Path $CertificateFileExportPath)
 {
-    Write-Host "Error: Invalid syntax for CertificateFileExportPath variable. Extension value in path should end with '.pfx'"
+    throw "Error: File($CertificateFileExportPath) already exist. Please remove to provide a different name."
 }
 
 # Create a self-signed certificate
 $ssc = New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $CertificateCN
+if ($ssc){
+    Write-Host "Certificate created successfully. Now exporting the certificate."
+}
+else {
+    throw "Error: Creation of certificate failed."
+}
+
 $crt = "cert:\localMachine\my\" + $ssc.Thumbprint
-$pwd = ConvertTo-SecureString -String $CertificateSecret -Force -AsPlainText
-Export-PfxCertificate -cert $crt -FilePath $CertificateFileExportPath -Password $pwd
+$pwd = ConvertTo-SecureString -String $CertificatePassword -Force -AsPlainText
+Export-PfxCertificate -cert $crt -FilePath $CertificateFileExportPath -Password $pwd -Force | Out-Null
+if (Test-Path -Path $CertificateFileExportPath) {
+    Write-Host "Certificate ($CertificateFileExportPath) exported successfully." 
+}
+else {
+    throw "Error: Export of certificate failed."
+}
+
