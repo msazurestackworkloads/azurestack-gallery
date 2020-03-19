27cfc36ea5598c092c9f96c89383dc400473cee7
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2020-03-19 11:23:04 -0700

    log collector only deals with kube-system (#200)
    * log collector only deals with kube-system
    
    * update release script

--- diagnosis/scripts/release.sh
+++ diagnosis/scripts/release.sh
@@ -4,5 +4,5 @@ VERSION=$1
 
 dos2unix *.sh
 mkdir -p _out
-tar -czf _out/diagnosis-v${VERSION}.tar.gz *.sh
-zip -q _out/diagnosis-v${VERSION}.zip *.sh
+tar -czf _out/diagnosis-v${VERSION}.tar.gz collectlogs.sh getkuberneteslogs.sh hosts.sh
+zip -q _out/diagnosis-v${VERSION}.zip collectlogs.sh getkuberneteslogs.sh hosts.sh
