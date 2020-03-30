de9e1bea41b89cff431ec6f2176f3a47703e6850
deaborch <44680211+deaborch@users.noreply.github.com>
2020-03-30 11:43:55 -0700

    fix: removed block internet feature flag

--- kubernetes/template/DeploymentTemplates/script.sh
+++ kubernetes/template/DeploymentTemplates/script.sh
@@ -248,9 +248,6 @@ generate_api_model()
         "customCloudProfile": {
             "portalURL": ""
         },
-        "featureFlags": {
-            "blockOutboundInternet": false
-        },
         "masterProfile": {
             "dnsPrefix": "",
             "distro": "",
@@ -548,22 +545,6 @@ if [ "$ENABLE_TILLER" == "true" ]; then
     > $AZURESTACK_CONFIGURATION_TEMP
     
     validate_and_restore_cluster_definition $AZURESTACK_CONFIGURATION_TEMP $AZURESTACK_CONFIGURATION || exit $ERR_API_MODEL
-fi
-
-
-
-#####################################################################################
-#disconnecte deployment
-
-if [ ! $DISCONNECTED_AKS_ENGINE_URL ]
-then
-    log_level -i "Blocking Outbound Connectivity"
-    cat $AZURESTACK_CONFIGURATION | \
-    jq '.properties.featureFlags.blockOutboundInternet = true' \
-    > $AZURESTACK_CONFIGURATION_TEMP
-    
-    validate_and_restore_cluster_definition $AZURESTACK_CONFIGURATION_TEMP $AZURESTACK_CONFIGURATION || exit $ERR_API_MODEL
-
 fi
 
 #####################################################################################
