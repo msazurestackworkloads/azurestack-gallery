ac482175220c591e501f1191db401b6558950a2a
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2020-04-13 15:03:33 -0700

    merge aks-e2e-release branch (#206)
    * Create aksEngineScale.sh
    
    * Create aksEngineUpgrade.sh
    
    * Create runAksScript.sh
    
    * Create MainTemplate.json
    
    * Create azuredeploy.json
    
    * Create azuredeploy.parameters.json
    
    * Create azurestack_template.json
    
    * Create script.sh
    
    * Create kubernetes-azurestack.json
    
    * feat: Add e2e test support in upstream test (#130)
    
    * skip sync time e2e test (#158)
    
    * skip sync time e2e test
    
    * Upgrade go version to 1.13.4 (#163)
    
    * Fixe2e (#164)
    
    * Upgrade go version to 1.13.4
    
    * Removing golang-dep from aks-e2e
    
    * Fix Go path
    
    * Set variable /c/Users/bhsantha.REDMOND for go version 1.13
    
    * Adding gobindata to PATH
    
    * e2e update kubectl
    
    * custom data generator
    
    * use k script instead of linking kubectl
    
    * capture error in log (#186)
    
    * Adding log to scale and upgrade operation
    
    * Fix for scale test results file name (#190)
    
    * ignore new e2e-test in v0.46.1 (#191)
    
    * Update azuredeploy.json to skip new e2e test (#192)
    
    update azuredeploy.json & handle scale-down
    
    * Use k script during scale & upgrade tests (#193)
    
    * Update path of cp command (#194)
    
    * Update azurestack_template.json
    
    Explicitly set masterProfile.availabilityProfile to AvailabilitySet
    
    * Fix azure cli install (#202)
    
    * Modify the download of microsoft signing key for  azure-cli installation
    
    * Modify the download of microsoft signing key for  azure-cli installation
    
    Co-authored-by: Bhuvaneswari Santharam <bhsantha@microsoft.com>
    
    * adding retry and fail fast for azure-cli installation (#203)
    
    Co-authored-by: Bhuvaneswari Santharam <bhsantha@microsoft.com>
    
    Co-authored-by: Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
    Co-authored-by: Rohit <rjaini@microsoft.com>
    Co-authored-by: haofan-ms <56602314+haofan-ms@users.noreply.github.com>
    Co-authored-by: Bhuvaneswari Santharam <bhsantha@microsoft.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,3 +3,4 @@
 mkdir -p _out
 _out/encoder ./kubernetes/template/DeploymentTemplates/script.sh > _out/customdata-k8s.txt
 _out/encoder ./registry/Scripts/script.sh > _out/customdata-registry.txt
+_out/encoder ./AKSEngine-E2E/Template/script.sh > _out/customdata-e2e.txt
